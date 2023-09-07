d6f44b4cf119389652e65426ebd83c955f2d14f5
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-09-08 00:49:11 +0530

    just added cards

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DisplayCard from '../Tool/displayCard'; 
 import Banner from '../Banner/Banner.js';
 import bannerImage from '../../Assests/Image/Banner3.jpg'
+import App from '../CollectionCard';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
      <div>
       <Banner imageSrc={bannerImage} text="Easy way to take loan" text1="Fast Loan" />
       <DisplayCard/>
+      <App/>
       </div>
       
       
