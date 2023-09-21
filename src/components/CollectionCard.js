cedb802712ae83bb89cae4bcd43f7a28df125e85
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-09-21 10:28:55 +0530

    Update CollectionCard.js

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import LoanCard from './MyCard';
+// import React from 'react';
+// import LoanCard from './MyCard';
 
-const App = () => {
-    const cardData = [
-        {
-            title: 'Card Title 1',
-            description: 'Description for Card 1 goes here.'
-        },
-        {
-            title: 'Card Title 2',
-            description: 'Description for Card 2 goes here.'
-        },
-        {
-            title: 'Card Title 3',
-            description: 'Description for Card 3 goes here.'
-        }
-    ];
+// const App = () => {
+//     const cardData = [
+//         {
+//             title: 'Card Title 1',
+//             description: 'Description for Card 1 goes here.'
+//         },
+//         {
+//             title: 'Card Title 2',
+//             description: 'Description for Card 2 goes here.'
+//         },
+//         {
+//             title: 'Card Title 3',
+//             description: 'Description for Card 3 goes here.'
+//         }
+//     ];
 
-    return (
-        <div className="container mx-auto p-8 flex justify-center">
-            {cardData.map((card, index) => (
-                <LoanCard 
-                    key={index} 
-                    title={card.title} 
-                    description={card.description} 
-                />
-            ))}
-        </div>
-    );
-}
+//     return (
+//         <div className="container mx-auto p-8 flex justify-center">
+//             {cardData.map((card, index) => (
+//                 <LoanCard 
+//                     key={index} 
+//                     title={card.title} 
+//                     description={card.description} 
+//                 />
+//             ))}
+//         </div>
+//     );
+// }
 
-export default App;
+// export default App;
