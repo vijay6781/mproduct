c1c07cdefdf8641dbdcb8ba8e0dc84821ab5e2a4
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-10-02 13:25:38 +0530

    added faq

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import DisplayCard from '../Tool/displayCard'; 
+// import DisplayCard from '../Tool/displayCard'; 
 import Banner from '../Banner/Banner.js';
 import bannerImage from '../../Assests/Image/Banner3.jpg'
 import CardList from '../MyCard';
 import WhyChooseBarbieLoan from '../Why';
 import '../Why/WhyChooseBarbieLoan.css';
+import LoanFAQSection from '../Faq/index.js';
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       {/* <DisplayCard/> */}
       <CardList/>
       <WhyChooseBarbieLoan/>
+      <LoanFAQSection/>
       </div>
       
       
--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,68 +1,67 @@
 import React from 'react';
 import { FaHome } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom'; 
-import { BsFillBuildingsFill,BsBuildingFillUp } from 'react-icons/bs';
-import {FaHandHoldingDollar} from 'react-icons/fa6'
-
+import { BsFillBuildingsFill, BsBuildingFillUp } from 'react-icons/bs';
+import { FaHandHoldingDollar } from 'react-icons/fa6';
 
 const Card = ({ title, description, icon }) => {
-    const navigate =useNavigate();
-  const handleApplyClick = () => {
-    navigate('/apply');
-    // Write your apply button click logic here
-  };
+    const navigate = useNavigate();
 
-  const renderIcon = () => {
-    switch (icon) {
-      case 'home':
-        return <FaHome className="text-3xl text-teal-200  absolute top-5 right-5" />;
-      case 'rupee':
-        return <FaHandHoldingDollar className=" text-4xl text-green-200  absolute top-5 right-5" />;
-      case 'business':
-        return <BsFillBuildingsFill className="text-3xl text-teal-200  absolute top-5 right-5" />;
-      case 'lap':
-        return <BsBuildingFillUp className="text-3xl text-teal-200  absolute top-5 right-5" />;  
-      default:
-        return null;
-    }
-  };
+    const handleApplyClick = () => {
+        navigate('/apply');
+    };
 
-  return (
-    <div className="max-w-sm w-80 mx-auto bg-white shadow-lg rounded-2xl mt-3 overflow-hidden mb-3 text-center">
-      <div className="px-6 py-4 relative">
-        {renderIcon()}
-        <div className="mb-4">
-          <h2 className="text-xl font-bold">{title}</h2>
+    const renderIcon = () => {
+        switch (icon) {
+            case 'home':
+                return <FaHome className="text-3xl text-teal-400 absolute top-5 right-5" />;
+            case 'rupee':
+                return <FaHandHoldingDollar className="text-4xl text-green-400 absolute top-5 right-5" />;
+            case 'business':
+                return <BsFillBuildingsFill className="text-3xl text-teal-400 absolute top-5 right-5" />;
+            case 'lap':
+                return <BsBuildingFillUp className="text-3xl text-teal-300 absolute top-5 right-5" />;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <div className="max-w-sm w-80 mx-auto bg-white shadow-lg rounded-2xl mt-3 overflow-hidden mb-3 text-center transition-transform transform hover:scale-105">
+            <div className="px-6 py-4 relative">
+                {renderIcon()}
+                <div className="mb-4">
+                    <h2 className="text-xl font-bold">{title}</h2>
+                </div>
+                <p className="text-gray-700 text-base">{description}</p>
+            </div>
+            <div className="px-6 py-4 flex justify-center">
+                <button
+                    className="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+                    onClick={handleApplyClick}
+                >
+                    Apply
+                </button>
+            </div>
         </div>
-        <p className="text-gray-700 text-base">{description}</p>
-      </div>
-      <div className="px-6 py-4 flex justify-center">
-        <button 
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={handleApplyClick}
-        >
-          Apply
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 const CardList = () => {
-  const cardsData = [
-    { title: 'Personal loan', description: 'Get personal loan at low interest within hours', icon: 'rupee' },
-    { title: 'Home loan', description: 'Get home loan at low interest rate ', icon: 'home' },
-    { title: 'Business Loan', description: 'Get business loan with low interst rate', icon: 'business' },
-    { title: 'LAP loan', description: 'Loan agains property with low interest rate ', icon: 'lap' },
-  ];
+    const cardsData = [
+        { title: 'Personal loan', description: 'Get personal loan at low interest within hours', icon: 'rupee' },
+        { title: 'Home loan', description: 'Get home loan at low interest rate ', icon: 'home' },
+        { title: 'Business Loan', description: 'Get business loan with low interest rate', icon: 'business' },
+        { title: 'LAP loan', description: 'Loan against property with low interest rate ', icon: 'lap' },
+    ];
 
-  return (
-    <div className="flex flex-wrap justify-center">
-      {cardsData.map((card, index) => (
-        <Card key={index} title={card.title} description={card.description} icon={card.icon} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="flex flex-wrap justify-center">
+            {cardsData.map((card, index) => (
+                <Card key={index} title={card.title} description={card.description} icon={card.icon} />
+            ))}
+        </div>
+    );
 };
 
 export default CardList;
