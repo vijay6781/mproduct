ea813cb709c80cb8f5624d8a8dbc1c8dd634bba2
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-12-27 00:10:03 +0530

    added form

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     
      <div>
-      <Banner imageSrc={bannerImage} text="India's trusted plateform for loan" text1="Easy way to  take quick loan" />
+      <Banner imageSrc={bannerImage} text="India's trusted plateform for loan" text1="Personal loan Start from 10.5%*" />
       {/* <DisplayCard/> */}
       <CardList/>
       <EMIcalculator/>
